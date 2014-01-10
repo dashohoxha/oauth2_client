85a231bbea475eb6c45541a70b07b4595a70a120
Dashamir Hoxha <dashohoxha@gmail.com>
2014-01-10 21:15:59 +0000

    Improved script for running tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+### Create the links of the required modules and libraries
+### on the profile 'testing', then run the tests.
 
-### get the path of profiles and contributed modules
-path=$(pwd)
-profiles=${path//\/btranslator*} 
-contrib=${path//\/oauth2_client*}
+cd $(dirname $0)
 
-### copy the modules that will be installed during the test to the profile 'testing'
-for module in oauth2_client oauth2_server entity entityreference xautoload ctools
-do 
-    ln -sf $contrib/$module $profiles/testing/modules/
+### get the paths
+drupal_dir=$(drush php-eval 'print realpath(".")')
+testing_dir=$drupal_dir/profiles/testing
+
+### list of the required modules (dependencies)
+module_list="
+    oauth2_client
+    oauth2_server
+    libraries
+    ctools
+    entity
+    entityreference
+    xautoload
+"
+### link the modules to the profile 'testing'
+for module in $module_list
+do
+    module_path=$(drush php-eval "print drupal_get_path('module', '$module')")
+    ln -sf $drupal_dir/$module_path $testing_dir/modules/
 done
+
+### link the required library oauth2-server-php
+oauth2_server_php=$(drush php-eval 'print libraries_get_path("oauth2-server-php")')
+mkdir -p $testing_dir/libraries/
+ln -sf $drupal_dir/$oauth2_server_php $testing_dir/libraries/
 
 ### run the tests
 drush test-clean
